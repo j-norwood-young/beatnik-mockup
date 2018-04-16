42630f74387d75c61a70aa5f41f1aab09c080900
Jason Norwood-Young <jason@10layer.com>
2018-04-16 10:04:17 +0200

    Big General Update

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,14 @@
+var _ = require('lodash');
+var $ = require('jquery');
+require('waypoints/lib/noframework.waypoints.min');
+
 document.addEventListener('DOMContentLoaded', function() {
-  // do your setup here
-  console.log('Initialized app');
+	// do your setup here
+	var waypoint = new Waypoint({
+		element: document.getElementsByClassName('headline')[0],
+	    handler: (direction) => {
+			console.log('waypoint', direction);
+		},
+	})	
 });
+
